fd3a60f0c7e7ad21541842b1d4b2e19dfef05f08
StenLmao <stenvor123@gmail.com>
2026-04-02 13:42:41 +0300

    ylesanne 2-6

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,21 @@
+import Button from "./UI/Button";
 const MealItem = (props) => {
+    const formattedPrice = new Intl.NumberFormat("et-EE", {
+        style: "currency",
+        currency: "EUR"
+    }).format(props.meal.price);
+
     return (
-        <li>
+        <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
-                    <p>{props.meal.description}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
+                    <p className="meal-item-description">{props.meal.description}</p>
                 </div>
-                <p>
-                    <button >Add to Cart</button>
+                <p className="meal-item-action">
+                    <Button>Add to Cart</Button>
                 </p>
             </article>
         </li>
