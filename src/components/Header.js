703f7160b436dfb62e675b21bcb1bed6793aa730
StenLmao <stenvor123@gmail.com>
2026-04-02 11:43:12 +0300

    ylesanne 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import logo from '../assets/logo.jpg'
 
 const Header = () => {
     return (
-        <header id="main-header">
-            <div id="title">
-                <img src={logo}/>
-                <h1>React Food Order App</h1>
-            </div>
-            <nav>
-            <button>Cart (0)</button>
-            </nav>
-        </header>
+            <header id="main-header">
+                <div id="title">
+                    <img src={logo} />
+                    <h1>React Food Order App</h1>
+                </div>
+                <nav>
+                    <button>Cart (0)</button>
+                </nav>
+            </header>
     )
 }
 
